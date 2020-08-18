16e9cde390b99a1544c5f56ffe1b2695e0f36eaa
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-08-18 22:04:17 +0300

    1

---
+++ app.js
@@ -0,0 +1,34 @@
+// alert ('Hello World')
+// alert ('1')
+// alert ('2')
+// alert ('3')
+
+// let age = 33;
+// let myAge = 37;
+// console.log(age + myAge);
+
+let car = {
+    color: 'redd',
+    maxSpeed: 250,
+    audio: {
+        brend: 'Sony',
+        speakers: 24
+    }
+};
+
+if(car.color == 'red') {
+    console.log('Автомобиль красный');
+}
+else if(car.color == 'green') {
+    console.log('Автомобиль зелёный');
+}
+else{
+    console.log('Цвет не определен');
+}
+
+
+// car.color = 'redd';
+
+// console.log(typeof car.audio);
+
+// console.log(car.audio);
